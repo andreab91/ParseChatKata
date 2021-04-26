04c47e760e968314bb8ed679676d19d09fe8dbd6
Andrea Bergamini <andrea.bergamini91@gmail.com>
2021-04-26 15:30:57 +0200

    Step 3 (two customer mentions as start)

--- chatParser.test.js
+++ chatParser.test.js
@@ -1,7 +1,7 @@
 const parse = require('./chatParser')
 
 describe('chat parser tests', () => {
-  describe('parse single sentence', () => {
+  describe('Step 1 (single sentence)', () => {
     test('with customer', () => {
       const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
       const expectedOutput = [{
@@ -27,7 +27,7 @@ describe('chat parser tests', () => {
     })
   })
 
-  test('parse two sentences', () => {
+  test('Step 2 (two sentences)', () => {
     const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
       '14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.'
     const expectedOutput = [{
@@ -40,6 +40,36 @@ describe('chat parser tests', () => {
       mention: '14:26:15 Agent : ',
       sentence: 'Aliquam non cursus erat, ut blandit lectus.',
       type: 'agent'
+    }]
+
+    expect(parse(input)).toEqual(expectedOutput)
+  })
+
+  test('Step 3 (two customer mentions as start)', () => {
+    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
+      '14:27:00 Customer : Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.\n' +
+      '14:27:47 Agent : Vestibulum tempor diam eu leo molestie eleifend.\n' +
+      '14:28:28 Customer : Contrary to popular belief, Lorem Ipsum is not simply random text.'
+    const expectedOutput = [{
+      date: '14:24:32',
+      mention: '14:24:32 Customer : ',
+      sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      type: 'customer'
+    }, {
+      date: '14:27:00',
+      mention: '14:27:00 Customer : ',
+      sentence: 'Pellentesque cursus maximus felis, pharetra porta purus aliquet viverra.',
+      type: 'customer'
+    }, {
+      date: '14:27:47',
+      mention: '14:27:47 Agent : ',
+      sentence: 'Vestibulum tempor diam eu leo molestie eleifend.',
+      type: 'agent'
+    }, {
+      date: '14:28:28',
+      mention: '14:28:28 Customer : ',
+      sentence: 'Contrary to popular belief, Lorem Ipsum is not simply random text.',
+      type: 'customer'
     }]
 
     expect(parse(input)).toEqual(expectedOutput)
