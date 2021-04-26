26e731512c3bb8f7bfb4ce523c773697bf6f591d
Andrea Bergamini <andrea.bergamini91@gmail.com>
2021-04-26 14:51:20 +0200

    Step 2 (two sentences)

--- chatParser.js
+++ chatParser.js
@@ -1,13 +1,19 @@
-function parse (line) {
+function parse (chat) {
+  const lines = chat.split('\n')
+
+  return lines.map(parseLine)
+}
+
+function parseLine (line) {
   const [mention, sentence] = line.split(' : ')
   const [date,] = mention.split(' ')
 
-  return [{
+  return {
     'date': date,
     'mention': mention + ' : ',
     'sentence': sentence,
     'type': isCustomer(line) ? 'customer' : 'agent'
-  }]
+  }
 }
 
 function isCustomer (line) {
--- chatParser.test.js
+++ chatParser.test.js
@@ -1,24 +1,44 @@
 const parse = require('./chatParser')
 
-describe('parse single sentence', () => {
-  test('with customer', () => {
-    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+describe('chat parser tests', () => {
+  describe('parse single sentence', () => {
+    test('with customer', () => {
+      const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      const expectedOutput = [{
+        date: '14:24:32',
+        mention: '14:24:32 Customer : ',
+        sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        type: 'customer'
+      }]
+
+      expect(parse(input)).toEqual(expectedOutput)
+    })
+
+    test('with agent', () => {
+      const input = '14:24:32 Agent : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
+      const expectedOutput = [{
+        date: '14:24:32',
+        mention: '14:24:32 Agent : ',
+        sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        type: 'agent'
+      }]
+
+      expect(parse(input)).toEqual(expectedOutput)
+    })
+  })
+
+  test('parse two sentences', () => {
+    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.\n' +
+      '14:26:15 Agent : Aliquam non cursus erat, ut blandit lectus.'
     const expectedOutput = [{
       date: '14:24:32',
       mention: '14:24:32 Customer : ',
       sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
       type: 'customer'
-    }]
-
-    expect(parse(input)).toEqual(expectedOutput)
-  })
-
-  test('with agent', () => {
-    const input = '14:24:32 Agent : Lorem ipsum dolor sit amet, consectetur adipiscing elit.'
-    const expectedOutput = [{
-      date: '14:24:32',
-      mention: '14:24:32 Agent : ',
-      sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    }, {
+      date: '14:26:15',
+      mention: '14:26:15 Agent : ',
+      sentence: 'Aliquam non cursus erat, ut blandit lectus.',
       type: 'agent'
     }]
 
