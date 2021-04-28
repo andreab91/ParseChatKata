ee569a1b1739c2d6fbbdbbd397541e72053cc1fc
Andrea Bergamini <andrea.bergamini91@gmail.com>
2021-04-28 09:14:03 +0200

    Step 5 (ignore extra dates)

--- chatParser.test.js
+++ chatParser.test.js
@@ -91,4 +91,21 @@ describe('chat parser tests', () => {
 
     expect(parse(input)).toEqual(expectedOutput)
   })
+
+  test('Step 5 (ignore extra dates)', () => {
+    const input = '14:24:32 Customer : Lorem ipsum dolor sit amet, consectetur adipiscing elit.14:26:15 Agent : I received it at 12:24:48, ut blandit lectus.'
+    const expectedOutput = [{
+      date: '14:24:32',
+      mention: '14:24:32 Customer : ',
+      sentence: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+      type: 'customer'
+    }, {
+      date: '14:26:15',
+      mention: '14:26:15 Agent : ',
+      sentence: 'I received it at 12:24:48, ut blandit lectus.',
+      type: 'agent'
+    }]
+
+    expect(parse(input)).toEqual(expectedOutput)
+  })
 })
